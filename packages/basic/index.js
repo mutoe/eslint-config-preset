688dbdc02b1865949eb8187c4ab9e6770bd1f357
mutoe <mutoe@foxmail.com>
2021-12-30 01:41:02 +0800

    fix: remove unicorn and prettier

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -12,11 +12,10 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'plugin:jsonc/recommended-with-jsonc',
     'plugin:yml/recommended',
-    'plugin:unicorn/recommended',
-    'prettier',
+    // 'plugin:unicorn/recommended',
     'plugin:jsx-a11y/recommended',
   ],
-  plugins: ['html', 'unicorn'],
+  plugins: ['html'],
   settings: {
     'import/resolver': {
       node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
@@ -111,7 +110,6 @@ module.exports = {
       'WithStatement',
     ],
     'no-await-in-loop': 0,
-    'unicorn/no-array-for-each': 0,
     'no-spaced-func': 'error',
     'object-curly-spacing': ['error', 'always'],
     'no-return-await': 'off',
@@ -165,40 +163,43 @@ module.exports = {
     'vars-on-top': 'error',
     'require-await': 'off',
     'no-return-assign': 'off',
-    // unicorns
-    // Pass error message when throwing errors
-    'unicorn/error-message': 'error',
-    // Uppercase regex escapes
-    'unicorn/escape-case': 'error',
-    // Array.isArray instead of instanceof
-    'unicorn/no-array-instanceof': 'error',
-    // Prevent deprecated `new Buffer()`
-    'unicorn/no-new-buffer': 'error',
-    // Keep regex literals safe!
-    'unicorn/no-unsafe-regex': 'off',
-    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
-    'unicorn/number-literal-case': 'error',
-    // ** instead of Math.pow()
-    'unicorn/prefer-exponentiation-operator': 'error',
-    // includes over indexOf when checking for existence
-    'unicorn/prefer-includes': 'error',
-    // String methods startsWith/endsWith instead of more complicated stuff
-    'unicorn/prefer-starts-ends-with': 'error',
-    // textContent instead of innerText
-    'unicorn/prefer-text-content': 'error',
-    // Enforce throwing type error when throwing error while checking typeof
-    'unicorn/prefer-type-error': 'error',
-    // Use new when throwing error
-    'unicorn/throw-new-error': 'error',
-    'unicorn/expiring-todo-comments': 'off',
+
+    // // unicorns
+    // // Pass error message when throwing errors
+    // 'unicorn/error-message': 'error',
+    // // Uppercase regex escapes
+    // 'unicorn/escape-case': 'error',
+    // // Array.isArray instead of instanceof
+    // 'unicorn/no-array-instanceof': 'error',
+    // // Prevent deprecated `new Buffer()`
+    // 'unicorn/no-new-buffer': 'error',
+    // // Keep regex literals safe!
+    // 'unicorn/no-unsafe-regex': 'off',
+    // // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
+    // 'unicorn/number-literal-case': 'error',
+    // // ** instead of Math.pow()
+    // 'unicorn/prefer-exponentiation-operator': 'error',
+    // // includes over indexOf when checking for existence
+    // 'unicorn/prefer-includes': 'error',
+    // // String methods startsWith/endsWith instead of more complicated stuff
+    // 'unicorn/prefer-starts-ends-with': 'error',
+    // // textContent instead of innerText
+    // 'unicorn/prefer-text-content': 'error',
+    // // Enforce throwing type error when throwing error while checking typeof
+    // 'unicorn/prefer-type-error': 'error',
+    // // Use new when throwing error
+    // 'unicorn/throw-new-error': 'error',
+    // 'unicorn/expiring-todo-comments': 'off',
+    // 'unicorn/no-array-for-each': 0,
+    // 'unicorn/no-process-exit': 0,
+    // 'unicorn/no-null': 0,
+    // 'unicorn/prevent-abbreviations': 0,
+
     'no-use-before-define': 0,
     'eslint-comments/disable-enable-pair': 'off',
     'import/prefer-default-export': 'off',
-    'unicorn/no-process-exit': 0,
     'import/extensions': 'off',
     'operator-linebreak': 0,
-    'unicorn/no-null': 0,
     'no-unused-expressions': 0,
-    'unicorn/prevent-abbreviations': 0,
   },
 }
