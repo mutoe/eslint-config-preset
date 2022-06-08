787698d16d08e421c7488c495df3c2a54a68ff58
mutoe <imutoe@gmail.com>
2022-06-09 02:35:33 +0800

    feat: support vue rules

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,25 +3,21 @@ module.exports = {
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module',
-      },
-      rules: {
-        // useless rules when 'vue/script-setup-uses-vars' is enabled
-        // 'no-unused-vars': 'off',
-        // '@typescript-eslint/no-unused-vars': 'off',
-      },
+      rules: {},
     },
   ],
   extends: [
+    '@vue/eslint-config-typescript/recommended',
     '@mutoe/eslint-config-preset-ts',
     'plugin:vue/vue3-recommended',
   ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
   rules: {
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
-    'vue/html-self-closing': 'off',
-    'vue/no-v-html': 'off',
-    'vue/script-setup-uses-vars': 'error',
+    'vue/no-v-html': 'warn',
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
   },
 }
