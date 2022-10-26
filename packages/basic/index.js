d3615e156d123c66f790ca24427894cbc8a52407
mutoe <imutoe@gmail.com>
2022-10-26 22:27:53 +0800

    feat: add jest config

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -78,7 +78,11 @@ module.exports = {
       },
     },
     {
-      files: ['**/*.{test,spec,cy}.?(c|m)[tj]s?(x)'],
+      files: ['**/*.{test,spec,cy}.?(c|m)[tj]s?(x)', '**/*.snap'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+      ],
       rules: {
         'prefer-arrow-callback': [
           'error',
@@ -92,7 +96,40 @@ module.exports = {
         'max-depth': ['warn', 10],
         'max-nested-callbacks': ['warn', 5],
         'max-statements': 'off',
-        'max-statements-per-line': ['warn', { max: 1 }],
+        'max-statements-per-line': 'off',
+        'n/no-callback-literal': 'off',
+
+        'jest/prefer-hooks-on-top': 'error',
+        'jest/prefer-todo': 'error',
+        'jest/require-top-level-describe': 'error',
+        'jest/no-test-return-statement': 'error',
+        'jest/no-duplicate-hooks': 'error',
+
+        'jest/max-expects': ['warn', { max: 6 }],
+        'jest/consistent-test-it': 'warn',
+        'jest/prefer-each': 'warn',
+        'jest/prefer-equality-matcher': 'warn',
+        'jest/prefer-comparison-matcher': 'warn',
+        'jest/prefer-mock-promise-shorthand': 'warn',
+        'jest/prefer-snapshot-hint': 'warn',
+        'jest/prefer-spy-on': 'warn',
+        'jest/prefer-called-with': 'warn',
+        'jest/no-conditional-in-test': 'warn',
+        'jest/no-restricted-matchers': ['warn', {
+          'not.toHaveBeenCalled': '`not.toHaveBeenCalled` are not always correct. Consider that it cannot be called due to a program error.\nYou should assert that what the program did.',
+          'not.toBeCalled': '`not.toBeCalled` are not always correct. Consider that it cannot be called due to a program error.\nYou should assert that what the program did.',
+          'not.toHaveBeenCalledWith': '`not.toHaveBeenCalledWith` are not always correct. Consider that it cannot be called due to a program error.\nYou should assert that what the program did.',
+          'not.toBeCalledWith': '`not.toBeCalledWith` are not always correct. Consider that it cannot be called due to a program error.\nYou should assert that what the program did.',
+        }],
+        'jest/prefer-lowercase-title': ['warn', {
+          ignore: ['describe', 'test'],
+          ignoreTopLevelDescribe: true,
+          allowedPrefixes: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        }],
+        'jest/no-large-snapshots': ['warn', {
+          maxSize: 50,
+          inlineMaxSize: 10,
+        }],
       },
     },
   ],
