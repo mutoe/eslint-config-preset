a3c7ffcb112b75867880b97f0b0dc83051e2f85e
mutoe <imutoe@gmail.com>
2022-10-26 22:30:45 +0800

    feat: ignore return type declaration in tests

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -68,6 +68,7 @@ module.exports = {
       rules: {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
   ],
