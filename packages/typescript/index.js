113e4b3b34a2d1321f195a9f0d2bf98b895b5984
mutoe <imutoe@gmail.com>
2023-01-29 23:33:11 +0800

    feat: allow to declare 'any' type directly

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -49,16 +49,13 @@ module.exports = {
         '@typescript-eslint/no-namespace': ['error', {
           allowDeclarations: true,
         }],
-        '@typescript-eslint/no-explicit-any': ['error', {
-          ignoreRestArgs: true,
-        }],
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
     {
       files: ['*.{test,spec,cy}.ts?(x)'],
       rules: {
         '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
