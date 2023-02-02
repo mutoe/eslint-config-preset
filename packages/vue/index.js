36231ba4dfa043464e0b402eb7b2361df2cd9a14
mutoe <imutoe@gmail.com>
2023-02-02 17:26:17 +0800

    feat: add vue a11y rules

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -12,6 +12,7 @@ module.exports = {
   extends: [
     '@mutoe/eslint-config-preset-ts',
     'plugin:vue/vue3-recommended',
+    'plugin:vuejs-accessibility/recommended',
   ],
   rules: {
     '@typescript-eslint/consistent-type-imports': 'error',
@@ -31,6 +32,29 @@ module.exports = {
       rules: {
         'no-undef': 'off',
         'func-call-spacing': 'off',
+
+        // https://github.com/vue-a11y/eslint-plugin-vuejs-accessibility/blob/dfc5d3433a8608edea9862e15f680ab9c9174e94/src/configs/recommended.ts#L14
+        'vuejs-accessibility/alt-text': 'warn',
+        'vuejs-accessibility/anchor-has-content': 'warn',
+        'vuejs-accessibility/aria-props': 'warn',
+        'vuejs-accessibility/aria-role': 'warn',
+        'vuejs-accessibility/aria-unsupported-elements': 'warn',
+        'vuejs-accessibility/click-events-have-key-events': 'warn',
+        'vuejs-accessibility/form-control-has-label': 'warn',
+        'vuejs-accessibility/heading-has-content': 'warn',
+        'vuejs-accessibility/iframe-has-title': 'warn',
+        'vuejs-accessibility/interactive-supports-focus': 'warn',
+        'vuejs-accessibility/label-has-for': 'warn',
+        'vuejs-accessibility/media-has-caption': 'warn',
+        'vuejs-accessibility/mouse-events-have-key-events': 'warn',
+        'vuejs-accessibility/no-access-key': 'warn',
+        'vuejs-accessibility/no-autofocus': 'warn',
+        'vuejs-accessibility/no-distracting-elements': 'warn',
+        'vuejs-accessibility/no-onchange': 'warn',
+        'vuejs-accessibility/no-redundant-roles': 'warn',
+        'vuejs-accessibility/no-static-element-interactions': 'warn',
+        'vuejs-accessibility/role-has-required-aria-props': 'warn',
+        'vuejs-accessibility/tabindex-no-positive': 'warn',
       },
     },
   ],
