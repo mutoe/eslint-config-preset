3f3f2ea34fa1ba3c0e9a7a4007a70878bf6bc50a
mutoe <imutoe@gmail.com>
2023-02-02 17:10:54 +0800

    fix: vue rules with setup sugar

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,28 +1,37 @@
 module.exports = {
-  overrides: [
-    {
-      files: ['*.vue'],
-      parser: 'vue-eslint-parser',
-      parserOptions: {
-        extraFileExtensions: ['.vue'],
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      rules: {},
-    },
-  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
   extends: [
     '@mutoe/eslint-config-preset-ts',
     'plugin:vue/vue3-recommended',
   ],
-  env: {
-    'vue/setup-compiler-macros': true,
-  },
   rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+
     'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
     'vue/no-v-html': 'warn',
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/consistent-type-imports': 'error',
+    'vue/no-setup-props-destructure': 'off',
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      env: {
+        node: false,
+        browser: true,
+      },
+      rules: {
+        'no-undef': 'off',
+        'func-call-spacing': 'off',
+      },
+    },
+  ],
 }
